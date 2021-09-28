e06ad043ffb216ddfc837309688fdbb580f6089e
Theo5607 <53973847+Theo5607@users.noreply.github.com>
2021-09-28 19:16:28 +0200

    Update client.py

--- client.py
+++ client.py
@@ -5,29 +5,18 @@ name=input('Quel est votre pseudo?')
 host, port = ('localhost', 5566)
 
 try:
-
     socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
     socket.connect((host, port))
-
     print("Client connecté")
 
     data = name
-
     data = data.encode("utf8")
-
     socket.sendall(data)
 
-    
-
     dataFromServer = socket.recv(1024)
-
     print(dataFromServer.decode());
 
-    
-
 except ConnectionRefusedError:
-
     print("connexion failed")
 
 finally:
